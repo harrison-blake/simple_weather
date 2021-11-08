064615f409e7a00091831087e49a8086692612ec
Harrison Blake <harrison.r.blake.gmail.com>
2021-11-07 17:25:33 -0700

    Add a bunch of stuff to current weather poro

---
+++ app/poros/current_weather.rb
@@ -0,0 +1,36 @@
+class CurrentWeather
+  attr_reader :time,
+              :date,
+              :sunrise,
+              :sunset,
+              :feels_like,
+              :temp,
+              :humidity,
+              :uvi,
+              :visibility,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    dt = data[:current][:dt]
+    sunrise = data[:current][:sunrise]
+    sunset = data[:current][:sunset]
+    offset = data[:timezone_offset]
+    @time = get_local_datetime(dt, offset).strftime("%I:%M %p")
+    @date = get_local_datetime(dt, offset).strftime("%m/%d/%Y")
+    @sunrise = get_local_datetime(sunrise, offset).strftime("%I:%M %p")
+    @sunset = get_local_datetime(sunset, offset).strftime("%I:%M %p")
+    @feels_like = data[:current][:feels_like]
+    @temp = data[:current][:temp]
+    @humidity = data[:current][:humidity]
+    @uvi = data[:current][:uvi]
+    @visibility = data[:current][:visibility]
+    @conditions = data[:current][:weather][0][:description]
+    @icon = data[:current][:weather][0][:icon]
+  end
+
+  def get_local_datetime(datetime, offset)
+    utc = Time.at(datetime).getgm
+    dt = utc.getlocal(offset)
+  end
+end
---
+++ spec/poros/current_weather_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe CurrentWeather do
+  describe 'happy path' do
+    it "should build a CurrentWeather PORO based on given data" do
+      data = []
+      lat_and_lon = [39.738453, -104.984853]
+
+      VCR.use_cassette('one_call_forecast') do
+        data = WeatherService.get_forecast(lat_and_lon)
+      end
+
+      current_weather = CurrentWeather.new(data)
+
+      expect(current_weather).to be_a(CurrentWeather)
+      expect(current_weather.time).to be_a(String)
+      expect(current_weather.date).to be_a(String)
+      expect(current_weather.sunrise).to be_a(String)
+      expect(current_weather.sunset).to be_a(String)
+      expect(current_weather.feels_like).to be_a(Float)
+      expect(current_weather.temp).to be_a(Float)
+      expect(current_weather.humidity).to be_a(Integer)
+      expect(current_weather.uvi).to be_a(Float)
+      expect(current_weather.visibility).to be_a(Integer)
+      expect(current_weather.conditions).to be_a(String)
+      expect(current_weather.icon).to be_a(String)
+      expect(current_weather.temp).to be_a(Float)
+    end
+
+    it "should return the correct time with given timezone offset" do
+      data = []
+        lat_and_lon = [39.2904, -76.6122]
+
+      VCR.use_cassette('baltimore_timezone_check') do
+        data = WeatherService.get_forecast(lat_and_lon)
+      end
+
+      current_weather = CurrentWeather.new(data)
+      expect(current_weather.time).to eq("05:36 PM")
+    end
+  end
+end
